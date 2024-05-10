d5c346afd342d4c6101a75abdd0f9fd9d7628e93
Allen Smithee <45022493+AllenSmithee@users.noreply.github.com>
2024-05-10 17:32:07 +0900

    1.2.3 Forward method at playercontroller.cs
    using class transform's translate method to move the object in update.

--- Assets/Course_01/Scripts/PlayerController.cs
+++ Assets/Course_01/Scripts/PlayerController.cs
@@ -7,15 +7,16 @@ namespace Course_01
 {
     public class PlayerController : MonoBehaviour
     {
-        
-        
 
-
-
-
-
-
+        void Update()
+        {
+            CourseMoveForward();
+        }
         //todo : make vehicle forward method
-
+        void CourseMoveForward()
+        {
+            //using transform position change to move forward
+            transform.Translate(Vector3.forward);
+        }
     }
 }
