ad04a49cef3ec8d54a682e069d9d5ae0bd60f5ac
Allen Smithee <45022493+AllenSmithee@users.noreply.github.com>
2024-05-10 16:51:45 +0900

    1.2.2 basic script structure explain
    in ide highlighted keywords are for the computer is made to understand.
    
    colon means the target class is inherits from other class.

--- Assets/Course_01/Scripts/PlayerController.cs
+++ Assets/Course_01/Scripts/PlayerController.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace Course_01
+{
+    public class PlayerController : MonoBehaviour
     {
         
+        
+
+
+
+
+
+
+        //todo : make vehicle forward method
+
     }
 }
