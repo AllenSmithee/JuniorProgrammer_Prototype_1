3627c1ffcedb40c88a5604017641367374a25d5c
Allen Smithee <45022493+AllenSmithee@users.noreply.github.com>
2024-05-13 16:20:27 +0900

    1.4.2 Get Input from Unity input manager
    GetAxsis("Stringname");

--- Assets/Course_01/Scripts/PlayerController.cs
+++ Assets/Course_01/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 
 
@@ -10,18 +11,33 @@ namespace Course_01
         [SerializeField] float m_speed = 5f;
         [SerializeField] float m_turnSpeed;
 
+        [SerializeField, ReadOnly] float m_horizontalInput;
+        [SerializeField, ReadOnly] float m_verticalInput;
+
+
         void Update()
         {
+            GetInputValue();
             CourseMoveForward();
         }
+
+        void GetInputValue()
+        {
+            m_horizontalInput = Input.GetAxis("Horizontal");
+            m_verticalInput = Input.GetAxis("Vertical");
+        }
+
+
         //todo : make vehicle forward method
         void CourseMoveForward()
         {
             //using transform position change to move forward
             //transform.Translate(Vector3.forward);
-            transform.Translate(Vector3.forward * Time.deltaTime * m_speed);
-            transform.Translate(Vector3.right * Time.deltaTime * m_turnSpeed);
-            
+            transform.Translate(Vector3.forward * Time.deltaTime * m_speed * m_verticalInput);
+
+            transform.Translate(Vector3.right * Time.deltaTime * m_turnSpeed * m_horizontalInput);
+
+
 
         }
     }
