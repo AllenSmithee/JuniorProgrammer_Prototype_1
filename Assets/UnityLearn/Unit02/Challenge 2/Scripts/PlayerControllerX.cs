139574431b838748e81092f9c5609012a7ebaa64
Allen Smithee <45022493+AllenSmithee@users.noreply.github.com>
2024-05-14 17:13:34 +0900

    done

--- Assets/UnityLearn/Unit02/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/UnityLearn/Unit02/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,24 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    [SerializeField] private float m_interval = 1.0f;
+    [SerializeField] private float m_lastPressTime = -1.0f;
+
+
     // Update is called once per frame
     void Update()
     {
+
+        // Prevent spamming
+        if (Time.time - m_lastPressTime < m_interval)
+        {
+            return;
+        }
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            m_lastPressTime = Time.time;
         }
     }
 }
